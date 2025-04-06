b389b443a16aeab5b836a366a749448da069afdb
Hitoshi-Noborikawa <dengchuanrenzhi@gmail.com>
2025-04-06 21:23:24 +0900

    initial commit

---
+++ lib/html/a11y/lint.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "lint/version"
+
+module Html
+  module A11y
+    module Lint
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/html/a11y/lint/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Html
+  module A11y
+    module Lint
+      VERSION = "0.1.0"
+    end
+  end
+end
